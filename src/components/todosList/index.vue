52f0825e46705c03779407a4e659eb5e314e16b6
Muhammed Mushtaha <muhamed.mushtaha@gmail.com>
2020-02-15 22:12:35 +0200

    set and get todos from localstorage, toggle isCompleted

--- src/components/todosList/index.vue
+++ src/components/todosList/index.vue
@@ -1,12 +1,22 @@
 <template>
   <div class="todos">
-    <h2 class="title">Todos</h2>
+    <h2 class="title">Todos List</h2>
     <AddTodo v-on:add-todo="addTodo" />
-    <TodosFilter v-if="todos.length !== 0" v-on:show-all="showAll" v-on:show-completed="showCompleted" />
+    <TodosFilter
+      v-if="todos.length !== 0"
+      v-on:show-all="showAll"
+      v-on:show-completed="showCompleted"
+      v-on:show-active="showActive"
+      :count="remainingTodosCount"
+    />
     <p class="no-todos" v-if="todos.length === 0">No todos.</p>
     <ul class="todos__list" v-if="todos.length !== 0">
       <li class="todos__list-item" v-for="todo in todos" v-bind:key="todo.id">
-        <TodoItem v-bind:todo="todo" v-on:delete-todo="deleteTodo" />
+        <TodoItem
+          :todo="todo"
+          v-on:delete-todo="deleteTodo"
+          v-on:toggle-is-completed="toggleIsCompleted"
+        />
       </li>
     </ul>
   </div>
@@ -26,27 +36,60 @@ export default {
   },
   data() {
     return {
-      todos: [],
-      allTodos: []
+      todos: []
     };
   },
+  mounted: function() {
+    this.todos = this.getTodosFromStorage();
+  },
+  computed: {
+    remainingTodosCount: function() {
+      return this.todos.filter(todo => !todo.isCompleted).length;
+    }
+  },
   methods: {
+    getTodosFromStorage: function() {
+      const todos = localStorage.getItem("todos") || "[]";
+      const parsedTodos = JSON.parse(todos);
+      return parsedTodos;
+    },
+    saveTodosToStorage: function(todos) {
+      localStorage.setItem("todos", JSON.stringify(todos));
+    },
     addTodo(todo) {
       this.todos.push({
         id: uuid.v4(),
         text: todo,
         isCompleted: false
       });
+      this.saveTodosToStorage(this.todos);
+    },
+    showAll() {
+      this.todos = this.getTodosFromStorage();
+    },
+    showCompleted() {
+      const todos = this.getTodosFromStorage();
+      this.todos = todos.filter(todo => todo.isCompleted);
+    },
+    showActive() {
+      const todos = this.getTodosFromStorage();
+      this.todos = todos.filter(todo => !todo.isCompleted);
     },
     deleteTodo(id) {
       this.todos = this.todos.filter(todo => todo.id !== id);
+      this.saveTodosToStorage(this.todos);
     },
-    showAll() {
-      this.todos = this.allTodos;
-    },
-    showCompleted() {
-      this.allTodos = this.todos;
-      this.todos = this.todos.filter(todo => todo.isCompleted);
+    toggleIsCompleted: function(id) {
+      this.todos = this.todos.map(todo => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            isCompleted: !todo.isCompleted
+          };
+        }
+        return todo;
+      });
+      this.saveTodosToStorage(this.todos);
     }
   }
 };
