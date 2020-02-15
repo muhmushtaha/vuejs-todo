00cd857bb5238d2709859da8e1671442fa762f87
Muhammed Mushtaha <muhamed.mushtaha@gmail.com>
2020-02-15 22:11:42 +0200

    add show active btn and show count with filter

--- src/components/todosFilter/index.vue
+++ src/components/todosFilter/index.vue
@@ -1,23 +1,44 @@
 <template>
-  <div class="filter-actions">
-    <button @click="showAll" class="action-btn">Show All</button>
-    <button @click="showCompleted" class="action-btn">Completed</button>
+  <div class="filter-section">
+    <div class="cont">
+      <p>{{count}} {{count | pluralized}} left</p>
+    </div>
+    <div class="filter-actions">
+      <button @click="showAll" class="action-btn">Show All</button>
+      <button @click="showActive" class="action-btn">Active</button>
+      <button @click="showCompleted" class="action-btn">Completed</button>
+    </div>
   </div>
 </template>
 <script>
 export default {
   name: "TodosFilter",
+  props: ["count"],
   methods: {
     showAll() {
       this.$emit("show-all");
     },
     showCompleted() {
       this.$emit("show-completed");
+    },
+    showActive() {
+      this.$emit("show-active");
+    }
+  },
+  filters: {
+    pluralized: function(count) {
+      return count === 1 ? "todo" : "todos";
     }
   }
 };
 </script>
 <style lang="scss" scoped>
+.filter-section {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 8px;
+}
 .filter-actions {
   text-align: left;
   margin-bottom: 16px;
